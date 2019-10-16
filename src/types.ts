ac8f54b55308a42599a2b55a3ea3e1d67b55d0b0
Eric Osterman <osterkraft@gmail.com>
2019-10-16 12:47:30 +0200

    Use nexus-prisma correctly within project

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,128 @@
-import { Photon } from '@generated/photon'
+import { queryType, mutationType, objectType, idArg, stringArg } from 'nexus'
 
-export interface Context {
-  photon: Photon
-}
+export const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.model.id()
+    t.model.name()
+    t.model.email()
+    t.model.posts({
+      pagination: false,
+    })
+  },
+})
+
+export const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.model.id()
+    t.model.createdAt()
+    t.model.updatedAt()
+    t.model.title()
+    t.model.content()
+    t.model.published()
+    t.model.author()
+    t.model.tags({
+      filtering: true
+    })
+  },
+})
+
+export const Tag = objectType({
+  name: 'Tag',
+  definition(t) {
+    t.model.id()
+    t.model.createdAt()
+    t.model.updatedAt()
+    t.model.name()
+  },
+})
+
+export const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.crud.post({
+      alias: 'post',
+    })
+
+    t.crud.posts({
+      alias: 'posts',
+      filtering: true
+    })
+
+    t.crud.users({
+      alias: 'users',
+    })
+
+    t.list.field('feed', {
+      type: 'Post',
+      resolve: (_, args, ctx) => {
+        return ctx.photon.posts.findMany({
+          where: { published: true },
+        })
+      },
+    })
+
+    t.list.field('filterPosts', {
+      type: 'Post',
+      args: {
+        searchString: stringArg({ nullable: true }),
+      },
+      resolve: (_, { searchString }, ctx) => {
+        return ctx.photon.posts.findMany({
+          where: {
+            OR: [
+              { title: { contains: searchString } },
+              { content: { contains: searchString } },
+            ],
+          },
+        })
+      },
+    })
+  },
+})
+
+export const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+    t.crud.createOneUser({ alias: 'signupUser' })
+    t.crud.deleteOnePost()
+
+    t.crud.createOneTag({ alias: 'createTag' })
+
+    t.field('createDraft', {
+      type: 'Post',
+      args: {
+        title: stringArg(),
+        content: stringArg({ nullable: true }),
+        authorEmail: stringArg(),
+      },
+      resolve: (_, { title, content, authorEmail }, ctx) => {
+        return ctx.photon.posts.create({
+          data: {
+            title,
+            content,
+            published: false,
+            author: {
+              connect: { email: authorEmail },
+            },
+          },
+        })
+      },
+    })
+
+    t.field('publish', {
+      type: 'Post',
+      nullable: true,
+      args: {
+        id: idArg(),
+      },
+      resolve: (_, { id }, ctx) => {
+        return ctx.photon.posts.update({
+          where: { id },
+          data: { published: true },
+        })
+      },
+    })
+  },
+})
